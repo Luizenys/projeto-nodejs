1b811c15c248ea27dc1ef46f2cd1c04142466b37
Luiza Rodrigues <luizars@id.uff.br>
2023-12-07 20:40:27 -0300

    streams

--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     const {method, url} = req
+
+    await json(req,res)
 
     if (method === 'GET' && url === '/users'){
         return res
-        .setHeader('Content-type', 'application/json')
         .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users'){
+        const {name, email} = req.body
+
         users.push({
             id: 1,
-            name: 'Luiza',
-            email: 'luiza@gmail.com'
+            name,
+            email
         })
         return res.writeHead(201).end()
     }
