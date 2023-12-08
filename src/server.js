38502fe0d6aab3715cdeee2e44dccdba7be21e43
Luiza Rodrigues <luizars@id.uff.br>
2023-12-08 20:19:53 -0300

    criando e persistindo banco de dados

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async(req, res) => {
     const {method, url} = req
@@ -9,6 +11,8 @@ const server = http.createServer(async(req, res) => {
     await json(req,res)
 
     if (method === 'GET' && url === '/users'){
+        const users = database.select('users')
+
         return res
         .end(JSON.stringify(users))
     }
@@ -16,11 +20,14 @@ const server = http.createServer(async(req, res) => {
     if (method === 'POST' && url === '/users'){
         const {name, email} = req.body
 
-        users.push({
-            id: 1,
+        const user = {
+            id: randomUUID(),
             name,
             email
-        })
+        }
+
+        database.insert('users', user)
+
         return res.writeHead(201).end()
     }
 
