00176999610389e2ecf6e60164864d44b709a21e
Luiza Rodrigues <luizars@id.uff.br>
2023-12-10 19:12:58 -0300

    creating routes and regex

---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import { Database } from './database.js'
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+    {
+        method: 'GET',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const users = database.select('users')
+            return res.end(JSON.stringify(users))
+        }
+    },
+    {
+        method: 'POST',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const {name, email} = req.body
+
+            const user = {
+                id: randomUUID(),
+                name,
+                email
+            }
+
+            database.insert('users', user)
+
+            return res.writeHead(201).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            return res.end()
+        }
+    }
+]
